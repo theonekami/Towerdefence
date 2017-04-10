a65c618f7d70a00dd3919ba8041c32177707ddcc
theonekami <theonekamisama@gmail.com>
2017-04-10 19:35:03 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace sf;
 
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -11,32 +12,61 @@ using namespace std;
 
 //enum dir{STOP=0,RIGHT,LEFT,UP,DOWN};
 
+//FIX THE TOWER PROBLEM!!SOMEHOW PLEAse!!!
+
 
 int path[]={1,1,1,1,1,1,1,4,4,4,4,4,4,1,1,1,1,1,1,4,4,4,4,1,1,1,1,1,1,1,0};
 
 RenderWindow w(VideoMode(800,664),"Td!!");
 
 
-bool call;
+bool call=false;
 int no=0;
 tower ta[50];
+Clock game_time;
 
-bool addtower(Texture &t)
+bool addtower(int t)
 {
-    bool y=ta[no].tower_hand(w,t);
-    if(!y)
+    Texture to;
+    stringstream scr;
+    string y;
+    switch(t)
+    {
+    case 1:
+        y="arrow";
+        break;
+    case 2:
+        y="magic";
+        break;
+    default:
+        return false;
+    }
+    scr<<"towers/"<<y<<".png";
+    to.loadFromFile(scr.str());
+    bool x=ta[no].tower_hand(w,to);
+    //cout<<x<<endl;
+    if(!x)
     {
         no++;
         w.setMouseCursorVisible(true);
     }
 
-    return y;
+    return x;
 }
 
-void drawtowers(Texture &t)
+
+void drawtowers()
 {
     for(int j=0;j<no;j++)
-    ta[j].tower_place(w,t);
+    ta[j].tower_place(w);
+}
+
+void call_creep(Creep &c)
+{
+    float t=0;
+    t=game_time.restart().asSeconds();
+    c.update(t,w);
+ //   c.Draw(w);
 }
 
 int main()
@@ -48,27 +78,26 @@ int main()
     bg.setTexture(m);
 
     Creep c(path);
-    Clock d;
-    float t=0;
 
     RectangleShape menu(Vector2f(800,64));
     menu.setFillColor(Color::Magenta);
     menu.setPosition(0,600);
 
     Texture to;
-    to.loadFromFile("towers/archer.png");
+    stringstream scr;
+    string y="arrow";
+    scr<<"towers/"<<y<<".png";
+    to.loadFromFile(scr.str());
 
     Sprite s;
     s.setTexture(to);
     s.setPosition(Vector2f(0,600));
 
-    Sprite x;
-    x.setOrigin(32,32);
-
+    int x=0;
 
     while(w.isOpen())
     {
-        t=d.restart().asSeconds();
+
         Vector2i mo=Mouse::getPosition(w);
         Event e;
         while (w.pollEvent(e))
@@ -80,18 +109,19 @@ int main()
             if(!call)
             {
 
-                    if (e.type==Event::MouseButtonPressed && e.mouseButton.button==Mouse::Left )
+                if (e.type==Event::MouseButtonPressed && e.mouseButton.button==Mouse::Left )
                 {
-                    if (mo.y>=600&&mo.x<=64)
+                    if (mo.y>=600)
                     {
+                        if(mo.x<=64)
+                            x=1;
+                        else if(mo.x<=64*2)
+                            x=2;
+                        else
+                            x=0;
                         call=true;
                         w.setMouseCursorVisible(false);
                     }
-                // else
-                    //{
-                    //   w.setMouseCursorVisible(true);
-                    //  call=false;
-                // }
 
                 }
             }
@@ -101,15 +131,16 @@ int main()
         w.draw(bg);
         w.draw(menu);
         w.draw(s);
+        drawtowers();
+        call_creep(c);
         if(call)
         {
-            call=addtower(to);
+            call=addtower(x);
         }
-        drawtowers(to);
-        cout<<no<<endl;
-        c.Draw(w);
-        c.update(t);
-        t=0;
+
+
+        //t=0;
+
         w.display();
     }
 
